b4d67db5156d9fd5965dc486ae019bec90b62b71
Igor Melo <igorjmelo4@gmail.com>
2019-09-30 23:25:31 -0300

    finishing project with final effects

--- 8.0_tinder_clone/mobile/src/index.js
+++ 8.0_tinder_clone/mobile/src/index.js
@@ -7,8 +7,13 @@
  */
 
 import React from 'react';
+import { YellowBox } from 'react-native';
 
 import Routes from './routes';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized Websocket'
+])''
 
 export default function App() {
   return (
--- 8.0_tinder_clone/mobile/src/pages/Main.js
+++ 8.0_tinder_clone/mobile/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import AsyncStorage from '@react-native-community/async-storage';
 import { SafeAreaView, View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
@@ -7,10 +8,12 @@ import api from '../service/api';
 import logo from '../assets/logo.png';
 import like from '../assets/like.png';
 import dislike from '../assets/dislike.png';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({ navigation }) {
     const id = navigation.getParam('user');
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(null);
 
     useEffect(() => {
         async function loadUsers() {
@@ -21,9 +24,17 @@ export default function Main({ navigation }) {
             });
             setUsers(response.data);
         }
-
         loadUsers();
+    }, [id]);
 
+    useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: { user: id }
+        });
+
+        socket.on('match', dev => {
+            setMatchDev(dev);
+        });
     }, [id]);
 
     async function handleLike() {
@@ -32,7 +43,6 @@ export default function Main({ navigation }) {
         await api.post(`/devs/${user._id}/likes`, null, {
             headers: { user: id },
         });
-
         setUsers(users.filter(rest));
     }
 
@@ -42,7 +52,6 @@ export default function Main({ navigation }) {
         await api.post(`/devs/${user._id}/dislikes`, null, {
             headers: { user: id },
         });
-
         setUsers(rest);
     }
 
@@ -81,6 +90,20 @@ export default function Main({ navigation }) {
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.button} onPress={handleLike}>
                         <Image source={like} />
+                    </TouchableOpacity>
+                </View>
+            )}
+
+            { matchDev && (
+                <View style={styles.matchContainer}>
+                    <Image style={styles.matchImage} source={itsamatch} />
+                    <Image style={styles.matchAvatar} source={{uri: matchDev.avatar }} />
+
+                    <Text style={styles.matchName}>{matchDev.name}</Text>
+                    <Text style={styles.matchBio}>{matchDev.bio}</Text>
+
+                    <TouchableOpacity onPress={() => setMatchDev(null)}>
+                        <Text style={styles.closeMatch}>FECHAR</Text>
                     </TouchableOpacity>
                 </View>
             )}
@@ -173,6 +196,50 @@ const styles = StyleSheet.cretea({
             width: 0,
             height: 2
         },
+    },
+
+    matchContainer: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'rgba(0,0,0,0.8)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    matchImage: {
+        height: 60,
+        resizeMode: 'contain',
+    },
+
+    matchAvatar: {
+        width: 160,
+        height: 160,
+        borderRadius: 80,
+        borderWidth: 5,
+        borderColor: '#FFF',
+        marginVertical: 30,
+    },
+
+    matchName: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        color: '#FFF',
+    },
+
+    matchBio: {
+        marginTop: 10,
+        fontSize: 16,
+        color: 'rgba(255,255,255,0.8)',
+        lineHeight: 24,
+        textAlign: 'center',
+        paddingHorizontal: 30
+    },
+
+    closeMatch: {
+        fontSize: 16,
+        color: 'rgba(255,255,255,0.8)',
+        textAlign: 'center',
+        marginTop: 30,
+        fontWeight: 'bold',
     },
 
 });
